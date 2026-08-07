beda50bc403b3e2e2603736e49dc3e3862de61b7
FantomeKGZ <deadgrog@gmail.com>
2026-08-07 16:30:53 +0600

    Подключить классификатор специальных схем

--- site/shared/js/navigation.js
+++ site/shared/js/navigation.js
@@ -206,11 +206,13 @@
   loadStyleOnce('../shared/css/favorites.css');
   loadStyleOnce('../shared/css/page-parameters.css');
   loadStyleOnce('../shared/css/esp32-panel.css');
+  loadStyleOnce('../shared/css/special-schemes.css');
   loadScriptOnce('../shared/js/home-content-loader.js');
   loadScriptOnce('../shared/js/fulltext-search.js');
   loadScriptOnce('../shared/js/recent-history.js');
   loadScriptOnce('../shared/js/favorites.js');
   loadScriptOnce('../shared/js/page-parameters.js');
+  loadScriptOnce('../shared/js/special-scheme-classifier.js');
   loadScriptOnce('../shared/js/esp32-client.js')
     .then(() => Promise.all([
       loadScriptOnce('../shared/js/esp32-panel.js'),
